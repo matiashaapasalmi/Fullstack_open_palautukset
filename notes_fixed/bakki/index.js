0b6ff0c4569e9abfd096fe61384af96a70ebd920
Matias <matias.haapasalmi@live.com>
2022-12-28 16:22:16 +0200

    tehty tiedosto korjauksia varten ei viela saatu maaliin

---
+++ notes_fixed/bakki/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+
+//const password = process.argv[2]
+const password = "jKcsXikJWF3nZlnG"
+
+
+const url =
+  `mongodb+srv://matiashaapasalmi:${password}@cluster0.tgrehpn.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+//vanha tapa ei mongoose tapa saada tietoa backista
+//app.get('/api/notes/:id', (request, response) => {
+  //const id = Number(request.params.id)
+  //const note = notes.find(note => note.id === id)
+  //if (note) {
+    //response.json(note)
+  //} else {
+    //response.status(404).end()
+  //}
+//})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+// kommentoitu nettiin vetoa varten
+
+//const PORT = process.env.PORT || 3001
+//app.listen(PORT, () => {
+  //console.log(`Server running on port ${PORT}`)
+//})
---
+++ notes_fixed/frontti/src/App.js
@@ -0,0 +1,129 @@
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import './index.css'
+
+const mongoose = require('mongoose')
+const password = "jKcsXikJWF3nZlnG"
+const url = 
+  `mongodb+srv://matiashaapasalmi:${password}@cluster0.tgrehpn.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+const Note = mongoose.model('Note', noteSchema)
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  
+  console.log('render', notes.length, 'notes') 
+
+  useEffect(() => {
+    noteService
+  .getAll()
+  .then(initialNotes => {
+    setNotes(initialNotes)
+  })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const Note = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5
+    }
+
+
+    //noteService
+      //.create(noteObject)
+      //.then(response => {
+        //setNotes(notes.concat(response.data))
+        //setNewNote('')
+      //})
+
+      noteObject.save().then(result => {
+        mongoose.connection.close()
+      })
+    }
+  
+    const handleNoteChange = (event) => {
+      console.log(event.target.value)
+      setNewNote(event.target.value)
+    }
+    
+
+  const Note = ({ note, toggleImportance }) => {
+    const label = note.important
+      ? 'make not important' : 'make important'
+  
+    return (
+      <li>
+        {note.content} 
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+    )
+  }
+
+
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>  
+      <ul>
+        {notes.map(note => 
+          <Note
+            key={note.id}
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul> 
+      
+      <form onSubmit={addNote}>
+        <input value={newNote}
+        onChange={handleNoteChange}
+                  />
+        <button type="submit">save</button>
+      </form>   
+      <Footer />
+    </div>
+  )
+}  
+export default App 
